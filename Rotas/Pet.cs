5be83f76d0ab343124dbec4d9979b06cdbc0d757
Mirahnda <vitoramiranda1417@gmail.com>
2025-05-03 12:46:08 -0300

    Desenvolvimento das classes principais(Usuario,Pet,Postagem)

---
+++ Rotas/Pet.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FurNet.Rotas
+{
+    public class Pet
+    {
+        public int Id {get; set;}
+        public string Nome {get; set;}
+        public string Raca {get; set;}
+        public int Idade {get; set;}
+        public double Peso {get; set;}
+
+        public int UsuarioId {get; set;}
+        public Usuario Usuario {get; set;}
+
+        public List<Postagem> Postagens {get; set;} = new();
+    }
+}
---
+++ Rotas/Postagem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FurNet.Rotas
+{
+    public class Postagem
+    {
+        public int Id {get; set;}
+        public string UrlMidia {get; set;}
+        public DateTime DataPostagem {get; set;} = DateTime.UtcNow;
+
+        public int PetId {get; set;}
+        public Pet Pet {get; set;}
+    }
+}

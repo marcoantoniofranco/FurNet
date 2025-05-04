6880752c553a4b0d3d89e8f43ad733ca0e4aab61
Marco <marco.a.f.811@gmail.com>
2025-05-04 03:51:38 -0300

    feat: Melhoria na organização das rotas

---
+++ Rotas/FurnetRota.cs
@@ -0,0 +1,9 @@
+﻿namespace FurNet.Rotas;
+
+public static class FurnetRota
+{
+    public static void FurnetRotas(this WebApplication app)
+    {
+        app.MapGet("home", () => "Bem vindo");
+    }
+}

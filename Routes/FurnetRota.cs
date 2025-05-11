ba1882c95adc797e2921806b0c9e898e877dc59e
Marco <marco.a.f.811@gmail.com>
2025-05-11 15:31:38 -0300

    feat: add endpoint GET para buscar usuario por ID

--- Routes/FurnetRota.cs
+++ Routes/FurnetRota.cs
@@ -10,6 +10,15 @@ public static class FurnetRota
         app.MapGet("/pets", (AppDbContext db) => {    
             var pets = db.Pets.ToList();
              return Results.Ok(pets);});
+             
+        app.MapGet("/usuarios/{id}", async (int id, AppDbContext db) =>{
+            var usuario = await db.Usuarios.FindAsync(id);
+            if (usuario == null)
+                return Results.NotFound("Usuario não encontrado");
+
+            usuario.Senha = null;
+            return Results.Ok(usuario);
+        });
 
     }
 }

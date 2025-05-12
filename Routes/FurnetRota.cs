c53a2d0dcb0980a2fa36f04bcdf8ce0f4e898381
Marco <marco.a.f.811@gmail.com>
2025-05-12 10:36:52 -0300

    Feat: Add criação de usuário

--- Routes/FurnetRota.cs
+++ Routes/FurnetRota.cs
@@ -1,4 +1,6 @@
 ﻿using FurNet.Data;
+using FurNet.Models;
+using FurNet.DTOs;
 
 namespace FurNet.Rotas;
 
@@ -14,10 +16,27 @@ public static class FurnetRota
         app.MapGet("/usuarios/{id}", async (int id, AppDbContext db) =>{
             var usuario = await db.Usuarios.FindAsync(id);
             if (usuario == null)
+            {
                 return Results.NotFound("Usuario não encontrado");
+            }
 
             usuario.Senha = null;
             return Results.Ok(usuario);
+        });
+
+        app.MapPost("/usuarios", async (CriarUsuarioDTO dto, AppDbContext db) =>
+        {
+            var usuario = new Usuario 
+            {
+                Nome = dto.Nome,
+                Email = dto.Email,
+                Senha = dto.Senha
+            };
+            
+            db.Usuarios.Add(usuario);
+            await db.SaveChangesAsync();
+            
+            return Results.Created($"/usuarios/{usuario.Id}", usuario);
         });
 
     }

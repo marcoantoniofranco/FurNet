3553a6e679991e2f7ac45370d3fc77b6b8c27eb4
Marco <marco.a.f.811@gmail.com>
2025-05-18 17:45:08 -0300

    Fix: Correção página login

--- Furnet-frontEnd/src/main.jsx
+++ Furnet-frontEnd/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import Quinn from './Quinn.jsx';
+import LoginPage from './LoginPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Quinn />
+    <LoginPage />
   </StrictMode>,
 );
